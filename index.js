6628936c5b276b8c44183cbf512e83aa2e9f006c
Aaron Kolker <akolker3@gmail.com>
2015-05-18 22:45:49 -0400

    Removed console.log

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 module.exports = {
 	sanity1: function () {
-		console.log('sanity 1 called');
+		//console.log('sanity 1 called');
 		return 0;
 	},
 	sanity2: function(){
-		console.log('sanity 2 called');
+		//console.log('sanity 2 called');
 		return "Sanity 2";
 	},
 	sanity3: function(){
-		console.log('sanity 3 called');
+		//console.log('sanity 3 called');
 		if (true == true) {
 			return true;
 		}
 	},
 	sanity4: function(a){
 		if (a == 0){
-			console.log('the value is 0');
+			//console.log('the value is 0');
 			return "0";
 		}
 		else if (a > 0) {
-			console.log('the value is positive');
+			//console.log('the value is positive');
 			return "positive";
 		}
 		else{
-			console.log('the value is negative')
+			//console.log('the value is negative')
 			return "negative";
 		}
 	},
